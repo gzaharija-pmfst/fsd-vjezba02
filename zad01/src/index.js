d90f7e932925f5cb524490bf71f59ff58a5c43a4
gzaharija <goran.zaharija@gmail.com>
2020-10-09 12:40:43 +0200

    Prvi commit

---
+++ zad01/src/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Kvaliteta usluge</h1>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
---
+++ zad02/src/index.js
@@ -0,0 +1,10 @@
+import React, {useState} from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => (
+  <div>
+    <h1>Izreka dana</h1>
+  </div>
+)
+
+ReactDOM.render(<App />, document.getElementById('root'))
